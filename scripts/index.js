f1e2d525221ba166c0c788daeb37a7e37354b547
mashkay <mashkay@gmail.com>
2024-05-06 22:00:51 +0200

    addcards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const plasesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function deleteCard(event) {
+  const card = event.target.closest('.card');
+  card.remove();
+}
 
-// @todo: Функция создания карточки
+function createCard(name, link, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardLikeButton = cardElement.querySelector('.card__like-button');
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+  cardDeleteButton.addEventListener('click', deleteCard);
+  cardLikeButton.addEventListener('click', function (event) {
+    event.target.classList.toggle('card__like-button_is-active');
+  });
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+  const cardElement = createCard(card.name, card.link, deleteCard);
+  plasesList.append(cardElement);
+});
