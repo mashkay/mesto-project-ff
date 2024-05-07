6af022c8bce0d9d44e618189d8b57c6a65d393e7
mashkay <mashkay@gmail.com>
2024-05-07 13:32:51 +0200

    card => cardData

--- scripts/index.js
+++ scripts/index.js
@@ -8,15 +8,15 @@ function toggleCardLike(likeButton) {
   likeButton.classList.toggle('card__like-button_is-active');
 }
 
-function createCard(card, cardDeletionFunction, cardLikeToggleFunction) {
+function createCard(cardData, cardDeletionFunction, cardLikeToggleFunction) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   const cardLikeButton = cardElement.querySelector('.card__like-button');
-  cardImage.src = card.link;
-  cardImage.alt = card.name;
-  cardTitle.textContent = card.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
   cardDeleteButton.addEventListener('click', () =>
     cardDeletionFunction(cardElement)
   );
